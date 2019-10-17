3a0110f83d1b74ea5655048b8746cbe477735c71
Charlotte Mothersole <charlotte.mothersole@brightminded.com>
2019-10-17 15:42:59 +0100

    create posts page

---
+++ wordpress/wp-content/themes/charlotte/template-posts.php
@@ -0,0 +1,28 @@
+<?php  
+/**
+ * Template Name: posts
+ * 
+ * @author         BrightMinded Ltd.
+ * @copyright      2017
+ * @license        Bespoke
+ */
+if( !defined( 'ABSPATH' ) ) { exit; }
+
+    get_header();
+?>
+
+<section id='posts'>    
+    <div id='posts-container'>
+        <div class='post-preview'>
+        </div>
+        <div class='post-preview'>
+        </div>
+    </div>
+    <div id='sidebar-container'>
+    </div>
+</section>
+
+<?php 
+/* to access footer.php*/
+get_footer();
+?>
