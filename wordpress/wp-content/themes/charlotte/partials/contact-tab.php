0ec456b13273407d6654e0df171752eb7acb2066
Charlotte Mothersole <charlotte.mothersole@brightminded.com>
2019-10-17 15:45:46 +0100

    add variables to contact tab

--- wordpress/wp-content/themes/charlotte/partials/contact-tab.php
+++ wordpress/wp-content/themes/charlotte/partials/contact-tab.php
@@ -2,14 +2,17 @@
 
 if( !defined( 'ABSPATH' ) ) { exit; }
 
-$url= get_field('contact_link');
-$toggle = get_field('contact_tab');
+$side_tab= get_field('side_tab');
+$url = $side_tab['url'];
+$toggle = $side_tab['side_toggle'];
+$text = $side_tab['side_text'];
+
 if($toggle){
 ?>    
 <a id='contact-tab' 
 class='contact-tab' 
 href='<?php echo $url ?>' 
-target='_blank'>Contact Me!</a>
+target='_blank'><?php echo $text ?></a>
 <?php
 }
 ?>
