5e94b2865cd559fea55f89d4e4558036e7376e93
Charlotte Mothersole <charlotte.mothersole@brightminded.com>
2019-10-21 17:24:08 +0100

    add single post php

--- wordpress/wp-content/themes/charlotte/single.php
+++ wordpress/wp-content/themes/charlotte/single.php
@@ -14,19 +14,70 @@ if( !defined( 'ABSPATH' ) ) { exit; }
 <section id='posts'>    
     <div class='single-post-container'>
         <div class='single-post'>
-            <h1>Header</h1>
-            <ul id='tags'>
-                <li>tag 1</li>
-                <li>tag 2</li>
-                <li>tag 3</li>
-            </ul>
-            <p> random textrandom textrandom textrandom textrandom textrandom textrandom textrandom textrandom text</p>
+            
+            
+            
+            <?php 
+                if ( have_posts() ) {
+                    while ( have_posts() ) {
+                 
+                        the_post(); ?>
+                        <?php the_post_thumbnail('category-hero'); ?>
+                        <h1 class='post-header'><?php the_title(); ?></h2>
+
+                        <div id='single-post-tags'>
+                            <?php $all_tags = the_tags('', ' ', ''); 
+                            
+                            ?>
+                        </div>
+                 
+                        <?php 
+                        the_content(); ?>
+                 
+                    <?php }
+                }
+            
+            ?>
+            
+            <p> random textrandom textrandom textrandom text
+            random textrandom textrandom textrandom textrandom text random textrandom textrandom textrandom text
+            random textrandom textrandom textrandom textrandom text random textrandom textrandom textrandom text
+            random textrandom textrandom textrandom textrandom text random textrandom textrandom textrandom text
+            random textrandom textrandom textrandom textrandom text</p>
+            <p> random textrandom textrandom textrandom text
+            random textrandom textrandom textrandom textrandom text random textrandom textrandom textrandom text
+            random textrandom textrandom textrandom textrandom text random textrandom textrandom textrandom text
+            random textrandom textrandom textrandom textrandom text random text random textrandom textrandom text
+            random text random textrandom text random textrandom text</p>
+            <div id='post-img2' class='post-img'></div>
+            <p> random textrandom textrandom textrandom text
+            random textrandom textrandom textrandom textrandom text random textrandom textrandom textrandom text
+            random textrandom textrandom textrandom textrandom text random textrandom textrandom textrandom text
+            random textrandom textrandom textrandom textrandom text random text random textrandom textrandom text
+            random text random textrandom text random textrandom text</p>
+            <div id='post-navigation'>
+                <a id='previous'>&laquo Previous</a>
+                <a id='next'>Next &raquo</a>
+            </div>
         </div>    
     </div>
     <div id='sidebar-container'>
         <div id='sidebar' class='sidebar-active'>
-            <p id='sidebar-exit'>Close
-            </p>
+            <p id='sidebar-exit'>Close</p>
+            <p id='sidebar-dots' class='hide'>...</p>
+            <ul id='sidebar-links'>
+                <a class='sidebar-link'><li>Latest Posts</li></a>
+                <a class='sidebar-link'><li>Archived Posts</li></a>
+                <li class='sidebar-list'>Posts By Category
+                    <ul id='category-links'>
+                        <a><li class='sidebar-link'>category</li></a>
+                        <a><li class='sidebar-link'>category</li></a>
+                        <a><li class='sidebar-link'>category</li></a>
+                    </ul>
+                </li>
+            </ul>
+            <div id='sidebar-footer'>
+            </div>
         </div>
     </div>
 </section>
