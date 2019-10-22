3a01ce75a036f1259dc00a7a9feb1adaff6cf6e4
Charlotte Mothersole <charlotte.mothersole@brightminded.com>
2019-10-22 17:20:29 +0100

    moved sidebar to partial template

---
+++ wordpress/wp-content/themes/charlotte/partials/sidebar.php
@@ -0,0 +1,32 @@
+<?php  
+
+if( !defined( 'ABSPATH' ) ) { exit; }
+
+$toggle_sidebar= get_field('show_sidebar');
+
+
+
+if($toggle_sidebar){
+?>    
+
+<div id='sidebar' class='sidebar-active'>
+    <p id='sidebar-exit'>Close</p>
+    <p id='sidebar-dots' class='hide'>...</p>
+    <ul id='sidebar-links'>
+        <li><a class='sidebar-link'>Latest Posts</a></li>
+        <li><a class='sidebar-link'>Archived Posts</a></li>
+        <li class='sidebar-list'>Posts By Category
+            <ul id='category-links'>
+                <li class='sidebar-link'><a>category</a></li>
+                <li class='sidebar-link'><a>category</a></li>
+                <li class='sidebar-link'><a>category</a></li>
+            </ul>
+        </li>
+    </ul>
+    <div id='sidebar-footer'>
+    </div>
+</div>
+    
+<?php
+}
+?>
