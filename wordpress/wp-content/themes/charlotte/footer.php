fee114ff4f6b90463da7af4fd73b3e2dd159a5e5
Charlotte Mothersole <charlotte.mothersole@brightminded.com>
2019-10-28 14:06:55 +0000

    add php and social media  to footer

--- wordpress/wp-content/themes/charlotte/footer.php
+++ wordpress/wp-content/themes/charlotte/footer.php
@@ -1,10 +1,28 @@
 <footer id='certifications'>
             <ul id='link-list'>
-                <li class='list-item'><a class='footer-item' id='profile-link' target='_blank' href='https://www.freecodecamp.org/fcc2334e54d-863a-4d45-884a-295371b6bb6a'>FreeCodeCamp Certifications</a></li>
-                <li class='list-item'><a class='footer-item' id='codecademy-link' target='_blank' href='https://www.codecademy.com/profiles/blog5446013551'>Codecademy Certifications</a></li>
-                <li class='list-item'><a class='footer-item' id='codewars-link' target='_blank' href='https://www.codewars.com/users/mcuriousity'>Codewars Certifications</a></li>
+                <?php 
+                $certifications = get_field('certifications');
+                $social_media = get_field('social_media');
+                foreach($certifications as $certification) {
+                    $cert_url = $certification['certifications_link'];
+                    $cert_name = $certification['certifications_name'];
+                    echo "<li class='list-item'><a class='footer-item' target='_blank' href='$cert_url'>$cert_name</a></li>";
+                }
+                ?>
             </ul>
-            <p id='footer-text'><img id='copyright' src='<?php /*function to access pathway*/ echo get_template_directory_uri() ?>/copyright.png'/>Made by Charlotte</p>         
+            <div id='footer-bottom'>
+                <p id='footer-text'><img id='copyright' src='<?php /*function to access pathway*/ echo get_template_directory_uri() ?>/copyright.png'/>Made by Charlotte</p>         
+                <div id='social'>
+                    <?php
+                    foreach($social_media as $social) {
+                        $social_url = $social['social_media_link'];
+                        $social_logo = $social['social_media_logo']['url'];
+                        $social_alt = $social['social_media_alt_text'];
+                        echo "<a href='$social_url' target='_blank'><img src='$social_logo' alt='$social_alt'></a>";
+                    }
+                    ?>
+                </div>
+            </div>
         </footer>
         <?php
             wp_footer();            
